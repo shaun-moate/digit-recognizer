b6b480963e573476be83c29bcb5f0b0df8c9722f
Shaun Moate <shaun.moate@gmail.com>
2023-08-26 22:38:53 +0200

    Went big(ger), moved to 300 epochs

---
+++ src/stages/submission.py
@@ -0,0 +1,93 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import csv
+import argparse
+
+from src.utils.load_params import load_params
+
+class Dataset(torch.utils.data.Dataset):
+  def __init__(self, data):
+        self.data = torch.load("data/processed/" + data)
+
+  def __len__(self):
+        return len(self.data)
+
+  def __getitem__(self, index):
+        X = self.data[index]
+        return X
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim = 1)
+
+def load_submission_data():
+    print("Loading the processed submission data...")
+    submission_data = Dataset("test_processed.pt")
+    print("Generating data and creating batches for submission...")
+    submission_loader = torch.utils.data.DataLoader(
+                      submission_data)
+    return submission_loader
+
+def submission():
+  with torch.no_grad():
+    for data in submission_loader:
+      output = network(data)
+      pred = output.data.max(1, keepdim=True)[1]
+      preds.append(pred.squeeze().tolist())
+  print(f"Predictions: {len(preds)}")
+
+if __name__ == "__main__":
+    print("---------------------------------------------------------------")
+    print(" SUBMISSION - START ---------------------------------------------")
+    print("---------------------------------------------------------------")
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("-c", dest="params", required=True)
+    args = arg_parser.parse_args()
+    params = load_params(args.params)
+
+    torch.backends.cudnn.enabled = params.train.cudnn_enabled
+    torch.manual_seed(params.base.random_seed)
+
+    if torch.backends.mps.is_available():
+        device = torch.device("mps")
+    elif torch.cuda.is_available():
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+
+    submission_loader = load_submission_data()
+
+    network = Net()
+    network.load_state_dict(torch.load('models/model.pth'))
+
+    preds = []
+    preds_formatted = []
+
+    submission()
+    for image, pred in enumerate(np.array(preds).flatten("F")):
+        preds_formatted.append([image+1, pred])
+
+    with open('reports/submission.csv', 'w+', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["ImageId", "Label"]) # headers
+        writer.writerows(preds_formatted) # Use writerows for nested list
+
+    print("---------------------------------------------------------------")
+    print(" SUBMISSION - COMPLETE ------------------------------------------")
+    print("---------------------------------------------------------------")
